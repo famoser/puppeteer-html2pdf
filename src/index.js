44af13fa3a90f9816f076464c9e49e748c59d5dc
Florian Moser <git@famoser.ch>
2023-08-22 22:16:08 +0200

    Add logging & improve API

--- src/index.js
+++ src/index.js
@@ -12,70 +12,73 @@ const port = 3000;
 
 const limit = process.env.BODY_LIMIT || '1mb';
 
-app.use(express.json({ limit }));
-app.use(bodyParser.text({ type: 'text/html', limit }));
+app.use(express.json({limit}));
+app.use(bodyParser.text({type: 'text/html', limit}));
 app.use(booleanParser());
 app.use(numberParser());
 
-async function print({ browser, htmlContents, options }) {
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time);
+  });
+}
+
+async function print({browser, htmlContents, options}) {
   const page = await browser.newPage();
-  await page.setContent(htmlContents, { waitUntil: 'networkidle0' });
+  page.on('console', message => console.log(`${message.type()} ${message.text()}`))
+    .on('pageerror', ({message}) => console.error(message))
+    .on('response', response => console.log(`${response.status()} ${response.url()}`))
+    .on('requestfailed', request => console.warn(`${request.failure().errorText} ${request.url()}`));
+
+  await page.setContent(htmlContents, {waitUntil: 'networkidle2'});
   return page.pdf(options);
 }
 
 function parseRequest(request) {
+  const headerAndBody = request.body.split('----- HEADER BODY -----');
+  const bodyAndFooter = headerAndBody[1].split('----- BODY FOOTER -----');
+
+  const margin = {
+    bottom: request.query.marginBottom ? request.query.marginBottom: 0,
+    left: request.query.marginLeft ? request.query.marginLeft : 0,
+    right: request.query.marginRight ? request.query.marginRight : 0,
+    top: request.query.marginTop ? request.query.marginTop : 0,
+  };
+
   return {
-    filename: (request.query.filename || 'document').replace(/(\.pdf)?$/, '.pdf'),                    // add .pdf extension if necessary
-    options: { format: 'a4', landscape: false, printBackground: true, ...request.query, path: null }  // discard potential `path` parameter
+    htmlContents: bodyAndFooter[0],
+    filename: (request.query.filename || 'document').replace(/(\.pdf)?$/, '.pdf'),
+    options: {format: 'a4', landscape: false, printBackground: true, headerTemplate: headerAndBody[0], footerTemplate: bodyAndFooter[1], margin, displayHeaderFooter: true, ...request.query, path: null}  // discard potential `path` parameter
   };
 }
 
 export function use(puppeteer) {
   function launchBrowser() {
     return puppeteer.launch({
+      dumpio: true,
       executablePath: 'google-chrome-stable',
       headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors']
     });
   }
 
   app.post('/', cors(), async (request, response) => {
-    const browser = await launchBrowser();
-    const { filename, options } = parseRequest(request);
-    const res = await print({ htmlContents: request.body, browser, options });
-    await browser.close();
-    response.attachment(`${filename}.pdf`).send(res);
-  });
-
-  app.post('/multiple', cors(), async (request, response) => {
-    const browser = await launchBrowser();
-    const { filename, options } = parseRequest(request);
-    const files = await Promise.all(request.body.map(htmlContents => {
-      const { name: path, removeCallback: rm } = tmp.fileSync();
-      return print({ htmlContents, browser, options: { ...options, path: path } }).then(() => ({ path, rm }));
-    }));
-
-    const res = files.reduce((merged, { path, rm }) => {
-      merged.addPagesOf(new pdf.ExternalDocument(fs.readFileSync(path)));
-      rm();
-      return merged;
-    }, new pdf.Document());
-
-    await browser.close();
-    const buffer = await res.asBuffer();
-    response.attachment(filename).send(buffer);
+    console.log(`Received request to print`, request.query);
+    try {
+      const browser = await launchBrowser();
+      const {htmlContents, filename, options} = parseRequest(request);
+      const res = await print({htmlContents, browser, options});
+      await browser.close();
+      response.attachment(`${filename}.pdf`).send(res);
+      console.log(`Printed`);
+    } catch (error) {
+      console.log('Error: ' + error);
+      response.status(500).send(error);
+    }
   });
 
   app.options('/*', cors());
 
-  /**
-   * Error-handling middleware always takes **four** arguments.
-   *
-   * You must provide four arguments to identify it as an error-handling middleware function.
-   * Even if you don’t need to use the next object, you must specify it to maintain the signature.
-   * Otherwise, the next object will be interpreted as regular middleware and will fail to handle errors.
-   * For details about error-handling middleware, see: https://expressjs.com/en/guide/error-handling.html.
-   */
   app.use((err, _, response, __) => {
     response.status(500).send(err.stack);
   });
